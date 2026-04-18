7f30659a00e8e6ad75c72d38a278921565f459a6
88B8 <egoboosted@proton.me>
2026-04-18 12:28:47 +0300

    создан entity-класс item

--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -23,7 +23,9 @@ import androidx.room.PrimaryKey
 /**
  * Entity data class represents a single row in the database.
  */
+@Entity(tableName = "items")
 data class Item(
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val name: String,
     val price: Double,
